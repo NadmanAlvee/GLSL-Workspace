bac71e2ce5069a59265edc081a4a5c9394afd84f
Nadman Alvee <nadmanalvee@gmail.com>
2026-06-09 09:14:07 +0600

    organic shape completed

--- final_project/src/js/index.js
+++ final_project/src/js/index.js
@@ -2,10 +2,17 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { HDRLoader } from "three/addons/loaders/HDRLoader.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 // shaders
 import vertexShader from "../shaders/vertex.glsl?raw";
+import vertexShaderMain from "../shaders/vertex_main.glsl?raw";
+import vertexShaderPars from "../shaders/vertex_pars.glsl?raw";
 import fragmentShader from "../shaders/fragment.glsl?raw";
+import fragmentShaderMain from "../shaders/fragment_main.glsl?raw";
+import fragmentShaderPars from "../shaders/fragment_pars.glsl?raw";
 
 class World {
   // constructor
@@ -26,6 +33,7 @@ class World {
     this.#initBackground();
 
     await this.#initObjects();
+    this.#createBloomEffect();
 
     this.#initAnimationLoop();
     this.#initResize();
@@ -56,7 +64,7 @@ class World {
       0.1,
       1000,
     );
-    camera.position.set(0, 2, 3);
+    camera.position.set(0, 3, 5);
     return camera;
   }
 
@@ -86,47 +94,98 @@ class World {
   }
   // Lights
   #initLights() {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+    const ambientLight = new THREE.AmbientLight("#7481ff", 0.5);
     this.scene?.add(ambientLight);
 
-    const directionalLight = new THREE.DirectionalLight("#ffffff", 2);
-    directionalLight.position.set(10, 20, 0);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.camera.bottom = -12;
+    const directionalLight = new THREE.DirectionalLight("#526cff", 0.6);
+    directionalLight.position.set(2, 2, 2);
     this.scene?.add(directionalLight);
   }
 
   // Initiate Objects
   async #initObjects() {
     // const geometry = new THREE.PlaneGeometry(2, 2, 2, 2);
-    const geometry = new THREE.IcosahedronGeometry(1, 100);
-    console.log(geometry);
+    const geometry = new THREE.IcosahedronGeometry(1, 300);
 
-    const material = new THREE.ShaderMaterial({
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
-      // wireframe: true,
+    const dummyTexture = new THREE.DataTexture(new Uint8Array([0]), 1, 1);
+    dummyTexture.needsUpdate = true;
+
+    const material = new THREE.MeshStandardMaterial({
+      displacementMap: dummyTexture,
+      onBeforeCompile: (shader) => {
+        // Store a reference to update uniforms in the animation loop
+        material.userData.shader = shader;
+        shader.uniforms.uTime = { value: 0 };
+
+        const parsTarget = /*glsl*/ `#include <displacementmap_pars_vertex>`;
+        shader.vertexShader = shader.vertexShader.replace(
+          parsTarget,
+          `${parsTarget}\nuniform float uTime;\n${vertexShaderPars}`,
+        );
+
+        const mainTarget = /*glsl*/ `#include <displacementmap_vertex>`;
+        shader.vertexShader = shader.vertexShader.replace(
+          mainTarget,
+          `${mainTarget}\n${vertexShaderMain}`,
+        );
+
+        const mainFragmentString = /*glsl*/ `#include <normal_fragment_maps>`;
+        shader.fragmentShader = shader.fragmentShader.replace(
+          mainFragmentString,
+          `${mainFragmentString}\n${fragmentShaderMain}`,
+        );
+
+        const parsFragmentString = /*glsl*/ `#include <bumpmap_pars_fragment>`;
+        shader.fragmentShader = shader.fragmentShader.replace(
+          parsFragmentString,
+          `${parsFragmentString}\n${fragmentShaderPars}`,
+        );
+
+        console.log(shader.fragmentShader);
+      },
     });
+    material.userData.shader = null;
     this.material = material;
-    material.uniforms.uTime = { value: 0 };
-    material.uniforms.uRadius = { value: 0.5 };
-    // material.uniforms.uTexture = {
-    //   value: new THREE.TextureLoader().load(colorfulTexture),
-    // };
-    console.log(material);
+    // console.log(this.material);
 
     const organicShape = new THREE.Mesh(geometry, material);
     this.scene.add(organicShape);
   }
 
+  #createBloomEffect() {
+    // Effect composer holds all pass
+    this.composer = new EffectComposer(this.renderer);
+
+    // represents the original colors of the scene - pass 1
+    const renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(renderPass);
+
+    // pass 2
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      2, // Strength (increased from 0.1)
+      0.05, // Radius
+      0.07, // threshold
+    );
+    this.composer.addPass(bloomPass);
+
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    // this.renderer.toneMappingExposure = 4;
+  }
+
   // Animate Scene
   #initAnimationLoop() {
-    this.renderer.setAnimationLoop((time) => {
+    const animate = (time) => {
       this.controls.update();
-      this.material.uniforms.uTime.value = time / 1000;
 
-      this.renderer.render(this.scene, this.camera);
-    });
+      if (this.material.userData.shader?.uniforms?.uTime) {
+        this.material.userData.shader.uniforms.uTime.value = time / 1000;
+      }
+
+      this.composer.render();
+      requestAnimationFrame(animate);
+    };
+    animate(performance.now());
   }
 
   // Handle Screen Resize
--- final_project/src/shaders/vertex_pars.glsl
+++ final_project/src/shaders/vertex_pars.glsl
@@ -1,13 +1,5 @@
-uniform float uTime;
-
-// v fro varying
-varying vec3 vPosition;
-varying vec3 vNormal;
-varying vec2 vUv;
+// uniform float uTime;
 varying float vDisplacement;
-
-// macros are always defined at compile time not runtime
-#define PI 3.1415926535897932384626433832795
 
 //	Classic Perlin 3D Noise 
 //	by Stefan Gustavson
@@ -96,25 +88,4 @@ float fit(float unscaled, float originalMin, float originalMax, float minAllowed
 
 float wave(vec3 position) {
   return fit(smoothMod(position.y * 6.0, 1.0, 1.5), 0.35, 0.6, 0.0, 1.0);
-}
-
-void main(){
-  // diplacement
-  vec3 coords = normal;
-  coords.y += uTime / 10.0; // animating
-  vec3 noisePattern = vec3(noise(coords));
-  float pattern = wave(noisePattern);
-  
-  // varyings
-  vPosition = position;
-  vNormal = normal;
-  vUv = uv;
-  vDisplacement = pattern;
-  float displacement = vDisplacement / 3.0;
-
-  // MVP
-  vec3 newPosition = position + normal * displacement;
-  vec4 modelViewPosition = modelViewMatrix * vec4( newPosition, 1.0 );
-  vec4 projectedPosition = projectionMatrix * modelViewPosition; 
-	gl_Position = projectedPosition;
 }
